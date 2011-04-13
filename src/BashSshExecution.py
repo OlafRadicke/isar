0122f3b29ae08c826de81ad392ae9b00fcef7b0a
Olaf Radicke <briefkasten@olaf-radicke.de>
2011-04-13 08:38:41 +0200

    begin with ssh support.

---
+++ src/BashSshExecution.py
@@ -0,0 +1,46 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+##
+#    Copyright (C) 2011  Olaf Radicke
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+## Class for execute commands in a ssh.
+class BashSshExecution:
+
+    ## ssh-server address
+    address = ""
+
+    ## ssh-user name
+    sshUser = ""
+
+    ## Constructor.
+    def __init__(self):
+
+    ## execute a bash command.
+    # @return Return result.
+    # except: subprocess.CalledProcessError
+    def do(self, _command):
+        print "[Command]:",_command
+        _command = "ssh -Y " + sshUser + "@" + address \
+            + " \"" + _command + "\""
+            
+        _f = subprocess.check_output(_command.split(),stderr=subprocess.STDOUT)
+        _out = ""
+        print "[_f]", _f
+        for _line in _f:
+            _out = _out + _line
+        return  _out    
